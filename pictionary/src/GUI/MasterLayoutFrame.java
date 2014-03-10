2b80659b07cb80adebb05ce2c1f0bbfd9f9fc1a2
woodardmc <woodardmc@hendrix.edu>
2014-03-09 21:43:26 -0500

    GUI Update
    -Added ActionListener to the chat entry field so it adds the text you
    type in on enter
    
    -added some more borders to make things look nice

--- pictionary/src/GUI/MasterLayoutFrame.java
+++ pictionary/src/GUI/MasterLayoutFrame.java
@@ -1,8 +1,11 @@
 package GUI;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.io.IOException;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.border.BevelBorder;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -17,7 +20,10 @@ public class MasterLayoutFrame extends JFrame{
 		setLayout(new FlowLayout());
 		
 		add(new LeftSidePanel());
-		add(new RightSidePanel());
+		RightSidePanel rightSide = new RightSidePanel();
+		rightSide.setBorder(BorderFactory.createLineBorder(Color.black));
+//		.createBevelBorder(BevelBorder.RAISED)
+		add(rightSide);
 		
 		pack();
 	}
--- pictionary/src/GUI/RightSidePanel.java
+++ pictionary/src/GUI/RightSidePanel.java
@@ -1,17 +1,25 @@
 package GUI;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 
 
 @SuppressWarnings("serial")
 public class RightSidePanel extends JPanel{
 	private JTextPane chatArea;
 	private JTextField chatEntryField;
+	private ArrayList<String> chatLog;
 	
 	public RightSidePanel(){
 		setPreferredSize(new Dimension(400, 700));
@@ -19,10 +27,48 @@ public class RightSidePanel extends JPanel{
 		
 		chatArea = new JTextPane();		
 		chatArea.setPreferredSize(new Dimension(300, 650));
+		chatArea.setEditable(false);
 		chatEntryField = new JTextField(10);
+		chatEntryField.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Chat"));
+		chatEntryField.addKeyListener(new EnterPressed());
+		
+		chatArea.setText("Welcome to the game!" + "\n" + "player1: this is the chat log" + "\n" + "player2: This is sort of what it should look like" + "\n");
 		
 		add(chatArea);
 		add(chatEntryField);
+	}
+	
+	public String getTextInArea(){
+		return chatEntryField.getText();
+	}
+	
+	private class EnterPressed implements KeyListener{
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			if (arg0.getKeyChar() == KeyEvent.VK_ENTER){
+				Document doc = chatArea.getDocument();
+				try {
+					doc.insertString(doc.getLength(), "player2: " + getTextInArea() + "\n", null);
+				} catch (BadLocationException e) {
+					e.printStackTrace();
+				}
+				chatEntryField.setText("");
+			}
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 
 }
